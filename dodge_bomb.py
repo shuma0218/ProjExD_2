a5750ea0a1610f1972fd907e4c86e8bd6eccf5f8
shuma <c0b220134e@edu.teu.ac.jp>
2024-11-26 13:38:47 +0900

    初期状態

---
+++ dodge_bomb.py
@@ -0,0 +1,51 @@
+import os
+import sys
+import pygame as pg
+
+
+WIDTH, HEIGHT = 1100, 650
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+
+def main():
+    pg.display.set_caption("逃げろ！こうかとん")
+    screen = pg.display.set_mode((WIDTH, HEIGHT))
+    bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg_img = pg.transform.scale(bg_img, (WIDTH, HEIGHT))    
+    kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = 300, 200
+    DELTA = {
+    pg.K_UP: (0, -5),
+    pg.K_DOWN: (0, 5),
+    pg.K_LEFT: (-5, 0),
+    pg.K_RIGHT: (5, 0),
+    }
+
+    clock = pg.time.Clock()
+    tmr = 0
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT: 
+                return
+        screen.blit(bg_img, [0, 0]) 
+
+        key_lst = pg.key.get_pressed()
+        sum_mv = [0, 0]
+        for key, delta in DELTA.items():
+            if key_lst[key]:
+                sum_mv[0] += delta[0]
+                sum_mv[1] += delta[1]
+
+        kk_rct.move_ip(sum_mv)
+        screen.blit(kk_img, kk_rct)
+        pg.display.update()
+        tmr += 1
+        clock.tick(50)
+
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
